3cd6d454f3065e631f0f0438c714d90f33357727
joemmalatesta <joemmalatesta@gmail.com>
2022-11-27 23:48:21 -0500

    Finished refresh functionality

--- script.js
+++ script.js
@@ -1,47 +1,68 @@
+//TODO: style the chart. Make the grid lines thicker and make it update the color of the line each time it is refreshed. 
+//TODO: Style buttons and fix into a responsive div / modal
+//TODO: look into obfuscation.
+
+
+let chart = document.getElementById("theChart").getContext("2d");
+let slope = 0;
+let chartData = [];
+let slopeAnswer
+
+
+let currentChart = new Chart(chart, {
+  type: "line",
+  data: {
+    labels: [-2, -1, 0, 1, 2],
+    datasets: [
+      {
+        label: "",
+        data: [slope * -2, slope * -1, 0, slope, slope * 2],
+        borderWidth: 8,
+      },
+    ],
+  },
+});
+
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-
 function refreshGraph() {
-  let slope = randomNumber(1, 6);
+  currentChart.destroy();
+  let slope = randomNumber(1, 6); // 1 through 5
   let coin = randomNumber(1, 3); //1 or 2
-if (coin == 1) {
-  slope -= slope * 2; //Make slope negative 
+  if (coin == 1) {
+    slope -= slope * 2; //Make slope negative
+  }
+  buildGraph(slope)
+  slopeAnswer = slope
 }
 
-  const chart = document.getElementById('theChart').getContext('2d');
-  new Chart(chart, {
-    type: 'line',
+
+function buildGraph(slope) {
+  currentChart = new Chart(chart, {
+    type: "line",
     data: {
-      labels: [-2,-1,0,1,2],
-      datasets: [{
-        label: '',
-        data: [slope*-2,slope*-1,0,slope, slope*2],
-        borderWidth: 8
-      }]
+      labels: [-2, -1, 0, 1, 2],
+      datasets: [
+        {
+          label: "",
+          data: [slope * -2, slope * -1, 0, slope, slope * 2],
+          borderWidth: 8,
+        },
+      ],
     },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: false
-        }
-      }
-    }
-  });;
-  return slope
+  });
 }
-
-
-const slope = refreshGraph()
-
 
 function verify() {
-  const ans = document.getElementById('slopeAnswer').value
-  if (ans == slope) {
-    alert("lets goo")
-  }
-  else{
-    alert("Meow meow meow")
+  const ans = document.getElementById("slopeAnswer").value;
+  if (ans == slopeAnswer) {
+    alert("lets goo");
+  } else {
+    alert("Meow meow meow");
   }
 }
+
+
+buildGraph(refreshGraph())
